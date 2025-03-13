18c7999304565649f0cac2e287c4b011c879004c
R0flan4k <klexak05@gmail.com>
2025-03-13 03:09:16 +0300

    Small changes.

--- SearchTree/include/search_tree_exceptions.h
+++ SearchTree/include/search_tree_exceptions.h
@@ -9,13 +9,11 @@ public:
     search_tree_error(const std::string &what_arg)
         : std::runtime_error(what_arg)
     {}
-    search_tree_error(const search_tree_error &) = default;
 };
 
 class wrong_input : public search_tree_error {
 public:
     wrong_input(const std::string &what_arg) : search_tree_error(what_arg) {}
-    wrong_input(const wrong_input &) = default;
 };
 
 } // namespace STExcepts
