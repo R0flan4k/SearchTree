30c3ac47302a76c20fe9321a619b9f1e21acaf32
R0flan4k <klexak05@gmail.com>
2025-03-17 21:22:05 +0300

    Small changes.

--- SearchTree/include/search_tree_exceptions.h
+++ SearchTree/include/search_tree_exceptions.h
@@ -9,9 +9,10 @@ public:
     search_tree_error(const std::string &what_arg)
         : std::runtime_error(what_arg)
     {}
+    virtual ~search_tree_error() = default;
 };
 
-class wrong_input : public search_tree_error {
+class wrong_input final : public search_tree_error {
 public:
     wrong_input(const std::string &what_arg) : search_tree_error(what_arg) {}
 };
